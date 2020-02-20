b2802a7be7025b9c3cb3b32e6033f1e7f2b3737b
carlosmonroyg <basesdedatosucc1920@gmail.com>
2020-02-19 20:02:27 -0500

    create class conection

--- src/services/Connection.java
+++ src/services/Connection.java
@@ -1,5 +1,33 @@
 package services;
 
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Connection {
+	private final int PORT = 8090;
+	private final String HOST = "LOCALHOST";
+	
+	
+	protected ServerSocket ss;
+	protected Socket s;
+	
+	
+	protected DataOutputStream outputServer, outputClient;
+	
+	
+public Connection(String type) {
+	
+	
+	if(type.equalsIgnoreCase("server")){
+		
+		ss = new ServerSoket(PORT);
+		s = new Socket();
+		
+	}
+	else {
+		s = new Socket (HOST, PORT);
+	}
+}
 
 }
